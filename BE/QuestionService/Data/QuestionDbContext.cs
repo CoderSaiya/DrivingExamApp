c189b00f3b185d00e69b7a6b5ed2dd3149547125
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2024-12-26 17:27:25 +0700

    add db context

---
+++ BE/QuestionService/Data/QuestionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuestionService.Models;
+
+namespace QuestionService.Data
+{
+    public class QuestionDbContext : DbContext
+    {
+        public QuestionDbContext(DbContextOptions<QuestionDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Question> Questions { get; set; }
+    }
+}
