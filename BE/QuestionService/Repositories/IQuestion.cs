957e76a900f510097ec3b52ed79d99089dfc5a04
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2024-12-26 17:34:29 +0700

    add repo

---
+++ BE/QuestionService/Repositories/IQuestion.cs
@@ -0,0 +1,10 @@
+﻿using QuestionService.Models;
+
+namespace QuestionService.Repositories
+{
+    public  interface IQuestion
+    {
+        Task<List<Question>> GetAllQuestions();
+        void AddQuestion(Question question);
+    }
+}
---
+++ BE/QuestionService/Repositories/QuestionRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuestionService.Data;
+using QuestionService.Models;
+
+namespace QuestionService.Repositories
+{
+    public class QuestionRepository : IQuestion
+    {
+        private readonly QuestionDbContext _context;
+
+        public QuestionRepository(QuestionDbContext context)
+        {
+            _context = context;
+        }
+
+        public async void AddQuestion(Question question)
+        {
+            await _context.Questions.AddAsync(question);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Question>> GetAllQuestions()
+        {
+            return await _context.Questions.ToListAsync();
+        }
+    }
+}
