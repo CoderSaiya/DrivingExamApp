19d2d1fee34e9b3f42cf6f8531ab56b38c17e7a7
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2024-12-26 16:51:24 +0700

    add models for question service

---
+++ BE/QuestionService/Models/Answer.cs
@@ -0,0 +1,9 @@
+﻿namespace QuestionService.Models
+{
+    public class Answer
+    {
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ BE/QuestionService/Models/Question.cs
@@ -0,0 +1,10 @@
+﻿namespace QuestionService.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public string? ImgUrl { get; set; }
+        public List<Answer> Answers { get; set; } = new List<Answer>();
+    }
+}
