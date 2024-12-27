d3d1cb7dd2c07372f88b4d3a180aa2b1dcbd8630
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2024-12-27 14:39:40 +0700

    add interface for message broker using rabbitmq

---
+++ BE/SharedLibrary/RabbitMq/IMessageBroker.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedLibrary.RabbitMq
+{
+    public interface IMessageBroker
+    {
+        void Publish(string queueName, string message);
+        void Subscribe(string queueName, Action<string> onMessageReceived);
+    }
+}
