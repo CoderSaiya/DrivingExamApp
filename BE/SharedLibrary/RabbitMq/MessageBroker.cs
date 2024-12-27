29c7f450fdec07888d169b2f782f729917302827
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2024-12-27 14:40:05 +0700

    add base implement

---
+++ BE/SharedLibrary/RabbitMq/MessageBroker.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace SharedLibrary.RabbitMq
+{
+    internal class MessageBroker : IMessageBroker
+    {
+        private readonly IConnection connection;
+        private readonly IModel channel;
+
+        public MessageBroker(IConnection connection, IModel channel)
+        {
+            this.connection = connection;
+            this.channel = channel;
+        }
+
+        public void Publish(string queueName, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe(string queueName, Action<string> onMessageReceived)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
